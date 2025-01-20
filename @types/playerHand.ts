10c5167afaae0fbeafb92b9dd597f5361d74e6e3
TigTagToad <taiga.arousse@gmail.com>
2025-01-20 15:12:28 +0000

    changed handcontext

--- @types/playerHand.ts
+++ @types/playerHand.ts
@@ -7,13 +7,19 @@ export interface Card {
     svg: string;    
 }
 
+
 export type Cards =  Card[]; 
+
+export interface Hand {
+    user_id: string;
+    cards: Card[];
+  };
 
 export type PlayerHandContextType = {
     user_id: string;
     pile_id: number;
     cards: Card[];
-    hand: Card[];
+    hand: Hand;
     addCard: (card: Cards) => void;
     returnToCard: (card: Card, pile_id: number) => void;
 
